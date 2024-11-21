6b0d4479c5d5b9f86685c6f3e6850027bb86cdcd
Gwen <bones2406@gmail.com>
2024-11-21 16:54:47 -0700

    starter content

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,27 @@
-import { Curtains, Plane } from "curtainsjs";
+import { Curtains, Plane } from "../node_modules/curtainsjs/src/index.mjs";
+
 window.addEventListener("load", () => {
-    
+    const curtains = new Curtains({
+        container: "canvas"
+    });
+
+    const planeElement = document.getElementsByClassName("plane")[0];
+
+    const params = {
+        vertexShaderID: planeElement.dataset.vsId,
+        fragmentShaderID: planeElement.dataset.fsId,
+        uniforms:{
+            time:{
+                name: "uTime",
+                type: "1f",
+                value: 0,
+            },
+        },
+    };
+
+    const plane = new Plane(curtains, planeElement, params);
+
+    plane.onRender(() => {
+        plane.uniforms.time.value++;
+    });
 });
