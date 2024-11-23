61ab02e8bfb27a4c14024b08428ca74e383c2616
Gwen <bones2406@gmail.com>
2024-11-22 23:33:42 -0700

    bundling

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    target: 'node',
+    entry: './scripts/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
